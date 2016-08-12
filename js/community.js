71cbc1d06b9fdb09bc3b93a80bd1d909453c5ece
Thomas Taylor <thomas.taylor@kineo.com>
2016-08-12 18:22:18 +0100

    Move stuff

--- js/community.js
+++ js/community.js
@@ -3,8 +3,6 @@ define(function(require) {
 	// toggle nav styling on scroll
 	$(document).scroll(function(event) {
 		var triggerAmount = $('.navigation').height()*1.5;
-		console.log(triggerAmount, $(event.currentTarget).scrollTop());
-
 		if($(event.currentTarget).scrollTop() > triggerAmount) {
 			$('.navigation').addClass('white');
 		} else {
